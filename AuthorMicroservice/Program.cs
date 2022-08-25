e5bd0c5a42649d9df0006dc6f5c909c511ed4d25
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-25 13:08:39 -0500

    services configured

--- AuthorMicroservice/Program.cs
+++ AuthorMicroservice/Program.cs
@@ -1,6 +1,11 @@
+using AuthorMicroservice.Data;
+using AuthorMicroservice.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- PublicationMicroservice/Program.cs
+++ PublicationMicroservice/Program.cs
@@ -1,6 +1,11 @@
+using PublicationMicroservice.Data;
+using PublicationMicroservice.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IBookService,BookService>();
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
